b9b3a440a8c641a8446b7826c029ef786ac26dd7
lyharthur <lyharthur@gmail.com>
2016-12-09 14:06:42 +0800

    add DHT11 sensor for T&H

---
+++ IoT_plant_care.ino
@@ -0,0 +1,64 @@
+
+
+#define BLYNK_PRINT Serial
+
+#include <dht.h>
+#include <BlynkSimpleYun.h>
+#include <SimpleTimer.h>
+
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "69ce88af6d0540728746cf4b9809357a";
+
+
+#define DHTPIN A0          // What digital pin we're connected to
+
+
+dht DHT;
+SimpleTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = DHT.humidity;
+  float t = DHT.temperature; 
+  DHT.read11(DHTPIN);
+  Serial.print("Current humidity = "); 
+  Serial.print(h); 
+  Serial.print("% "); 
+  Serial.print("temperature = ");
+  Serial.print(t); 
+  Serial.println("C ");
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+}
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth);
+
+
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+}
+
+
+
